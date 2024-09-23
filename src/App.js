6be89be92bf817bad7cac0e862213a9d1e8880c9
Shah61 <119098657+Shah61@users.noreply.github.com>
2024-09-23 20:59:43 +0800

    Add basic routing to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
-  return (
+ return(
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      <Route path="/" element={<div>Listing Page</div>} />
+      <Route path="/product/:id" element={<div>Product Page</div>} />
+      </Routes>
     </div>
-  );
+  </Router>
+ );
 }
 
 export default App;
