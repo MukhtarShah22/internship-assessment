a9c87878a62ec74b385976fb457052c929884de4
Shah61 <119098657+Shah61@users.noreply.github.com>
2024-09-23 21:14:16 +0800

    Implement product fetching in ListingPage

--- src/pages/ListingPage.js
+++ src/pages/ListingPage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+import { response } from "express";
 
 function ListingPage(){
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+        .then(response => {
+            setProducts(response.data);
+            setLoading(false);
+        })
+        .catch(error =>{
+            setError('Error fetching products.');
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{error}</div>;
+
     return(
         <div>
             <h1>Product Listing</h1>
